07f67a07196b03faf0b1fb9113f24cebb043aa9f
aiandrox <aiandrox7@gmail.com>
2023-10-04 20:12:30 +0900

    細かい修正

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
+            <a href="#" className="mr-4 hover:underline md:mr-6">
               マイリスト
             </a>
           </li>
