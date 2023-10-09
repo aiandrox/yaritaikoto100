6fc9f38eeaf1d63e0270edef3645cf2f2955a5a6
aiandrox <aiandrox7@gmail.com>
2023-10-09 16:27:39 +0900

    localstrageの値が取得できていないwip

--- src/app/callbacks/page.ts
+++ src/app/callbacks/page.ts
@@ -1,11 +1,20 @@
-import { cookies } from "next/headers";
+"use client";
 
 import { redirect } from "next/navigation";
 import { useCallbackFacade } from "./callbacks.facade";
+import useLocalStorage from "@/utils/localStorage";
 
-export default async function handler() {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("access_token");
-  console.log(accessToken);
+export default function Callbacks() {
+  const { value: localStorageItemsValue } = useLocalStorage("items");
+  console.log(localStorageItemsValue);
+  const localStorageItems = localStorageItemsValue ? JSON.parse(localStorageItemsValue) : [];
+  const items = Array.from({ length: 100 }, (_, i) => {
+    return localStorageItems ? (localStorageItems[i] as string) : "";
+  });
+
+  const { handleCreateList } = useCallbackFacade();
+
+  handleCreateList(items);
+
   redirect("/");
 }
